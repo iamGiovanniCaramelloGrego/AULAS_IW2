963e7fd3c9a82318a2a1901312532d12647c9349
Giovanni Caramello <100931407+iamGiovanniCaramelloGrego@users.noreply.github.com>
2023-05-09 12:12:46 -0300

    09/05 basico

---
+++ salamalenko/teste.js
@@ -0,0 +1,35 @@
+//alert("aaaaaaaa")
+
+//var nome = prompt("digite seu nome: ")
+//console.log(nome)
+//document.write(nome)
+
+//let a = 10
+//console.log(a)
+
+//if (true){
+//    let a = 15
+//    console.log(a)
+//}
+//console.log(a)
+
+var nome = prompt("digite o seu nome: ")
+//alert("bem vindo " + nome)
+// TEMPLETE STRING => INTERPRETAÇAO
+alert(`Bom Dia ${nome}`)
+
+var n1 = parseInt(prompt("Digite n1: "))
+var n2 = parseInt(prompt("Digite n2: "))
+console.log("n1 é" + typeof n1)
+console.log("n2 é" + typeof n2)
+
+var soma = (n1+n2)
+
+console.log(" a soma é: " + typeof soma)
+
+document.write(`<p>A soma é ${soma}</p>`)
+//console.log("a soma é " + soma)
+//console.log(typeof n1)
+
+//Template String
+//console.log(`A soma é ${soma}`)
